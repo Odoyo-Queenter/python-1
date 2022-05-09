cdcd63a89f4c460b58913f3674df834df87f268e
Odoyo-Queenter <achiengodoyoqueenter@gmail.com>
2022-05-09 09:25:09 +0300

    first commit

---
+++ python1.py
@@ -0,0 +1,21 @@
+#Introduction
+#01_Day_Introduction
+
+print(3 + 7)   #Addition
+print(10 / 2)  # Division
+print(4 * 4)   # Multiplication
+print(10 - 5)  #Subtraction
+print(10 % 3)  #Modulus
+print(10 // 2) #Floor division operator
+print(4 ** 4)  #Exponential
+
+#checking datatypes
+
+print(type(20))                 #int
+print(type(10.5))               #Float
+print(type(10.5+5j))            # complex
+print(type("Queenter Achieng")) #String
+print(type(type([1,2,3,4,5])))  #List
+print(type((1,2,3,4,5)))        #Tuple
+print(type({10.5,12.0,6,7}))    #Set
+print(type({'name':"Faith"}))   #Dictionary
